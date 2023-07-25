f746249d0c64d461a8ecee8ae2aaf9bb9e2d7783
MilvaTech <webmilva@gmail.com>
2023-07-25 13:25:05 -0700

    commit num 2

--- src/main/java/com/sta/spTest02/SpTest02Application.java
+++ src/main/java/com/sta/spTest02/SpTest02Application.java
@@ -1,12 +1,17 @@
 package com.sta.spTest02;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpTest02Application {
-
+	private static final Logger log = LoggerFactory.getLogger(SpTest02Application.class);
 	public static void main(String[] args) {
+        log.info("---------------------------------------------");
+		log.info("log in the main class");
+		log.info("---------------------------------------------");
 		SpringApplication.run(SpTest02Application.class, args);
 	}
 
