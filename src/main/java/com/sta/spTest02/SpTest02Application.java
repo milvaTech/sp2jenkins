757cfc2b612dcac6af65701eeef196e36abee6a1
MilvaTech <webmilva@gmail.com>
2023-07-25 12:32:06 -0700

    commit num 1

---
+++ src/main/java/com/sta/spTest02/SpTest02Application.java
@@ -0,0 +1,13 @@
+package com.sta.spTest02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpTest02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpTest02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sta/spTest02/SpTest02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sta.spTest02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpTest02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
